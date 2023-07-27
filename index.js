aceba75e5c7cd41671b5e7dbe49e6d7c8346bdd3
Aloysius David <aloysiusdavid@airasia.com>
2023-07-27 17:28:31 +0800

    Updat index.js

--- index.js
+++ index.js
@@ -6,24 +6,18 @@ const cors = require("cors");
 const express = require("express");
 const router = express.Router();
 
-router.post("/events", (req, res) => {
+router.post("/", (req, res) => {
   let url =
     "https://ap-southeast-1.aws.data.mongodb-api.com/app/data-rqzyr/endpoint/data/v1/action/find";
   console.log("[POST] " + url);
-
-  const postData = JSON.stringify({
-    collection: "Thugs Ultimate Events",
-    database: "ultimate_events",
-    dataSource: "Cluster0",
-  });
+  const postData = JSON.stringify(req.body);
 
   nodeFetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Request-Headers": "*",
-      "api-key":
-        "xd12Uz1jPNghWAhmBnHhlfpAnhM8ypv3mH8Fx7OeJOTl7mIlpzOrGVQp0HplVL21",
+      "api-key": req.get('api-key'),
     },
     body: postData,
   })
